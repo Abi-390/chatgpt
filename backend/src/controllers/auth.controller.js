fd7cf441d1f68c8419f75fdcf87f2b1b50c6ba3c
Abi-390 <dekaabinash07@gmail.com>
2026-01-28 19:44:30 +0530

    Register api done

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,7 +1,45 @@
-const userModel = require("../models/user.model")
+const userModel = require("../models/user.model");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
+async function registerUser(req, res) {
+  const {
+    fullName: { firstName, lastName },
+    email,
+    password,
+  } = req.body;
 
-async function registerController(req,res){
+  const isUserAlreadyExist = await userModel.findOne({ email });
 
-    const{fullName:{firstName,lastName},email,password} = req.body;
+  if (isUserAlreadyExist) {
+    res.status(400).json({
+      message: "User already exists",
+    });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await userModel.create({
+    fullName: { firstName, lastName },
+    email,
+    password: hashedPassword,
+  });
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+  res.cookie("token", token);
+
+  res.status(201).json({
+    message: "User registered successfully",
+    user: {
+      email: user.email,
+      _id: user._id,
+      fullName: user.fullName,
+    },
+  });
 }
+
+
+
+
+module.exports = {registerUser}
