df3376ac9a334389e6249cf2413ae9d721f73204
Abi-390 <dekaabinash07@gmail.com>
2026-01-28 21:46:13 +0530

    auth middleware done

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const authRoutes = require("./routes/auth.routes")
+
+/*Routes*/
+const authRoutes = require("./routes/auth.routes");
+const chatRoutes = require("./routes/chat.routes");
 
 
 const app = express();
+
+/* middlewares */
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/auth",authRoutes)
+/* using routes */
+app.use("/api/auth",authRoutes);
+app.use("/api/chat",chatRoutes)
 
 module.exports = app;
---
+++ backend/src/routes/chat.routes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const authMiddleware = require("../middlewares/auth.middleware")
+
+
+const router = express.Router();
+
+
+router.post("/",authMiddleware.authUser);
+
+
+
+module.exports = router;
