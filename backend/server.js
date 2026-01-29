79cc3a77d215ed96acef750e2d6eb02bf77f52ae
Abi-390 <dekaabinash07@gmail.com>
2026-01-29 22:16:25 +0530

    socket middleware auth check done

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const app = require("./src/app")
 const connectToDb = require("./src/db/db")
-
+const initSocketServer = require("./src/sockets/socket.server")
+const httpServer = require("http").createServer(app);
 
 connectToDb();
+initSocketServer(httpServer);
 
-app.listen(3000,()=>{
+httpServer.listen(3000,()=>{
     console.log("Server is running at port 3000")
 })
--- backend/src/sockets/socket.server.js
+++ backend/src/sockets/socket.server.js
@@ -1,12 +1,34 @@
 const { Server } = require("socket.io");
+const cookie = require("cookie");
+const jwt = require("jsonwebtoken");
+const userModel = require("../models/user.model");
 
-function initSocketServer(httpServer){
-    const io = new Server(httpServer,{});
+function initSocketServer(httpServer) {
+  const io = new Server(httpServer, {});
 
-    io.on("connection",(socket)=>{
-        console.log("New socket connection:",socket.id)
-    })
+  io.use(async (socket, next) => {
+    const cookies = cookie.parse(socket.handshake.headers?.cookie || "");
+
+    if (!cookies.token) {
+      next(new Error("Authentication error:No token provided"));
+    }
+
+    try {
+      const decoded = jwt.verify(cookies.token, process.env.JWT_SECRET);
+
+      const user = await userModel.findById(decoded.id);
+
+      socket.user = user;
+
+      next();
+    } catch (error) {
+      next(new Error("Authentication error:Invalid token"));
+    }
+  });
+
+  io.on("connection", (socket) => {
+    console.log("New socket connection:", socket.id);
+  });
 }
 
-
-module.exports = initSocketServer;  
+module.exports = initSocketServer;
