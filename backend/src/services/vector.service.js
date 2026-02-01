0f665d7abb2cf38ed0d5cd1fdf1da493fc42448d
Abi-390 <dekaabinash07@gmail.com>
2026-02-01 19:38:50 +0530

    Pinecone vector database added to service

---
+++ backend/src/services/vector.service.js
@@ -0,0 +1,27 @@
+const { Pinecone } = require("@pinecone-database/pinecone");
+
+const pc = new Pinecone({ apiKey: process.env.PINECONE_API_KEY });
+
+const chatgptIndex = pc.Index("chat-gpt");
+
+async function createMemory({ vectors, metadata, messageId }) {
+  await chatgptIndex.upsert([
+    {
+      id: messageId,
+      values: vectors,
+      metadata,
+    },
+  ]);
+}
+
+async function queryMemory({ queryVector, limit = 5, metadata }) {
+  const data = await chatgptIndex.query({
+    vector: queryVector,
+    topK: limit, // means after vector search taking the closest 5 vector points(limit=5) to the search result of the query
+    filter: metadata ? { metadata } : undefined,
+    includeMetadata: true,
+  });
+  return data.matches;
+}
+
+module.exports = { createMemory, queryMemory };
