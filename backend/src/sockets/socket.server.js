7b58714c9645e496158b25ff6b92fa7b753ed98d
Abi-390 <dekaabinash07@gmail.com>
2026-02-03 20:53:28 +0530

    optimized both saving message to db fn and generating vector

--- backend/src/sockets/socket.server.js
+++ backend/src/sockets/socket.server.js
@@ -36,22 +36,53 @@ function initSocketServer(httpServer) {
     socket.on("ai-message", async (messagePayload) => {
       console.log("Payload message sent to  AI :", messagePayload);
 
-      await messageModel.create({
+    /* const message = await messageModel.create({
         chat: messagePayload.chat,
         user: socket.user._id,
         content: messagePayload.content,
         role: "user",
       });
 
-      const vectors = await aiService.generateVector(messagePayload.content);
+      const vectors = await aiService.generateVector(messagePayload.content);*/
 
-      const memory = await queryMemory({
+      const [message,vectors] = await Promise.all([
+        messageModel.create({
+        chat: messagePayload.chat,
+        user: socket.user._id,
+        content: messagePayload.content,
+        role: "user",
+      }),
+
+      aiService.generateVector(messagePayload.content)
+
+      ])
+
+      /*const memory = await queryMemory({
         queryVector: vectors[0].values,
         limit: 3,
-       /* metadata: {
-          user: socket.user._id.toString(),
-        },*/
-      });
+        metadata: {
+         
+        },
+      }); */
+
+      const [memory,chatHistory] = await Promise.all([
+         queryMemory({
+        queryVector: vectors[0].values,
+        limit: 3,
+        metadata: {
+         user:socket.user._id
+        },
+      }),
+
+      messageModel
+        .find({
+          chat: messagePayload.chat,
+        })
+        .sort({ createdAt: -1 })
+        .limit(10)
+        .lean()
+
+      ])
 
       await createMemory({
         vectors,
@@ -65,13 +96,13 @@ function initSocketServer(httpServer) {
 
       console.log(memory);
 
-      const chatHistory = await messageModel
+      /*const chatHistory = await messageModel
         .find({
           chat: messagePayload.chat,
         })
         .sort({ createdAt: -1 })
         .limit(10)
-        .lean();
+        .lean();*/
 
       const stm = chatHistory.reverse().map((item) => {
         return {
@@ -102,14 +133,24 @@ function initSocketServer(httpServer) {
       const response = await aiService.generateResponse([...ltm, ...stm]);
       console.log("AI response:", response);
 
-      await messageModel.create({
+     /*const responseMessage = await messageModel.create({
         chat: messagePayload.chat,
         user: socket.user._id,
         content: response,
         role: "model",
-      });
+      });*/
 
-      const responseVectors = await aiService.generateVector(response);
+     /* const responseVectors = await aiService.generateVector(response);*/
+
+     const [responseMessage,responseVectors] = await Promise.all([
+        messageModel.create({
+        chat: messagePayload.chat,
+        user: socket.user._id,
+        content: response,
+        role: "model",
+      }),
+        await aiService.generateVector(response)
+     ])
 
       await createMemory({
         vectors: responseVectors,
