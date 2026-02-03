f96cb1b1b82104b7401efce4f8fbcaf9951ab362
Abi-390 <dekaabinash07@gmail.com>
2026-02-03 23:46:28 +0530

    Frontend complete

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,7 @@ const app = express();
 /* middlewares */
 // CORS Configuration
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "laughableai.vercel.app",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
